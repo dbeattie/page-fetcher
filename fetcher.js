a891bf4b42348c998be64a3677b88acc4681b33c
dbeattie <darren@fettle.ca>
2019-12-04 22:11:22 +0000

    attempted some of the stretch and read through some node.js documentation

--- fetcher.js
+++ fetcher.js
@@ -4,6 +4,7 @@ const path = process.argv[3];
 
 //this is using request and fs modules
 const request = require('request');
+//const readline = require('readline');
 const fs = require('fs');
 
 
@@ -21,3 +22,20 @@ request(url, (error, response, body) => {
     }) 
   });
 });
+
+/*Optional / Stretch: If the file already exists, let the user know and 
+prompt them to type in Y(followed by the enter key) to overwrite the 
+file, otherwise skip and exit the app. We suggest using the readline 
+module, which we've previously used.
+
+What should happen if the local file path given is invalid?
+
+Indeed! We should check to make sure that it's valid. 
+Otherwise we should fail and let the user know about this issue.
+
+What should happen if the given URL results in an error or non-200 
+result?
+
+Wow, you're on fire! In that case it would make sense to terminate 
+the app explaining to the user what went wrong, and not write the 
+response body to the file.*/
